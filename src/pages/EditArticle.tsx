e2245c4f0367536c6cfb0f892be139a7a29c6895
poboy-dev <ekoulepaulpatrice@gmail.com>
2026-01-08 17:11:17 +0100

    Changes to be committed: 	modified:   src/pages/EditArticle.tsx

--- src/pages/EditArticle.tsx
+++ src/pages/EditArticle.tsx
@@ -2,6 +2,7 @@ import { ArticleForm } from "@/components/ArticleForm";
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
 import { Article } from "@/types/article";
+import { useParams } from "react-router-dom";
 
 const EditArticle = () => {
   const { id } = useParams();
